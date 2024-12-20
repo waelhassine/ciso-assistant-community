646c8b2ca0ec6699047106089a1f253517742ee2
Wael Hassine <wael.hassine0@gmail.com>
2024-12-20 14:09:22 +0100

    Update compose yml

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,9 +1,16 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
-echo "Running database migrations..."
-poetry run python manage.py migrate
-
-echo "Creating superuser account..."
-poetry run python manage.py createsuperuser --no-input || echo "Superuser already exists."
-
-echo "Initialization complete."
+if [ -f db/ciso-assistant.sqlite3 ] ; then
+    echo "the database seems already created"
+    echo "you should launch docker compose up -d"
+    echo "for clean start, you can remove the database file, run docker compose down and then docker compose rm and start again"
+else
+    docker rmi ghcr.io/intuitem/ciso-assistant-community/backend:latest ghcr.io/intuitem/ciso-assistant-community/frontend:latest 2> /dev/null
+    docker compose up -d
+    echo "Giving some time for the database to be ready, please wait ..."
+    sleep 30
+    echo "initialize your superuser account..."
+    docker compose exec backend poetry run python manage.py createsuperuser
+    echo "connect to ciso assistant on https://localhost:8443"
+    echo "for successive runs you can now use docker compose up"
+fi
